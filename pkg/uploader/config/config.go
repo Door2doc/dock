2ee1005755bbcba6934af6be9cac0f535aca7291
Yigal Duppen <yigal@publysher.nl>
2019-03-07 16:10:34 +0100

    Complete service harness

--- pkg/uploader/config/config.go
+++ pkg/uploader/config/config.go
@@ -3,6 +3,7 @@ package config
 
 import (
 	"context"
+	"time"
 )
 
 // ValidationResult contains the results of a single step in validating the current configuration.
@@ -12,24 +13,41 @@ type ValidationResult struct {
 
 // Configuration contains the configuration options for the service.
 type Configuration struct {
-	Username string // username to connect to the d2d upload service
-	Password string // password to connect to the d2d upload service
-	DSN      string // DSN for the database connection to retrieve visitor information from
-	Query    string // Query to execute to retrieve visitor information
+	// username to connect to the d2d upload service
+	Username string
+	// password to connect to the d2d upload service
+	Password string
+	// DSN for the database connection to retrieve visitor information from
+	DSN string
+	// Query to execute to retrieve visitor information
+	Query string
+	// Set to true if the service should be active
+	Active bool
+	// Pause between runs
+	Interval time.Duration
 }
 
 // Load loads the configuration from a well-known location. It does not give an error if the configuration
 // does not exist.
 func Load(ctx context.Context) (*Configuration, error) {
-	panic("todo")
+	// todo
+	return &Configuration{}, nil
 }
 
 // Validate validates the configuration and returns the results of those checks.
 func (c *Configuration) Validate(ctx context.Context) []ValidationResult {
-	panic("todo")
+	// todo
+	return nil
 }
 
 // Save stores the latest configuration values to a well-known location.
 func (c *Configuration) Save() error {
-	panic("todo")
+	// todo
+	return nil
+}
+
+// Refresh ensures that the configuration is the latest version saved.
+func (c *Configuration) Refresh() error {
+	// todo
+	return nil
 }
--- pkg/uploader/service.go
+++ pkg/uploader/service.go
@@ -2,15 +2,18 @@ package uploader
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/kardianos/service"
+	"github.com/publysher/d2d-uploader/pkg/uploader/config"
 )
 
 // Service contains the definition to run the application as a service.
 type Service struct {
 	shutdown context.CancelFunc
 	log      service.Logger
+	srv      *http.Server
 }
 
 // NewService creates a new Service instance.
@@ -20,15 +23,18 @@ func NewService() *Service {
 
 // Start starts running the service. It will return as soon as possible.
 func (s *Service) Start(svc service.Service) error {
+	// obtain logger
 	logger, err := svc.Logger(nil)
 	if err != nil {
 		return err
 	}
 	s.log = logger
 
+	// create service context
 	ctx, cancel := context.WithCancel(context.Background())
 	s.shutdown = cancel
 
+	// run service asynchronously
 	go func() {
 		err := s.run(ctx)
 		switch {
@@ -37,29 +43,81 @@ func (s *Service) Start(svc service.Service) error {
 			_ = logger.Info("Service stopped")
 		case err != nil:
 			_ = logger.Errorf("While running service: %v", err)
-		}
-		if err != nil {
-			_ = logger.Errorf("While running service: %v", err)
+			cancel()
 		}
 	}()
-	return nil
-}
 
-func (s *Service) run(ctx context.Context) error {
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-time.Tick(time.Second):
-			_ = s.log.Infof("tick")
-		}
+	// start HTTP server for configuration purposes
+	s.srv = &http.Server{
+		Addr:    ":17226",
+		Handler: http.DefaultServeMux,
 	}
+
+	// run HTTP server
+	go func() {
+		_ = s.log.Info("Starting configuration server")
+		err := s.srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			_ = logger.Errorf("Failed to start configuration server: %v", err)
+			_ = svc.Stop()
+		}
+	}()
+
+	return nil
 }
 
 // Stop stops the currently running service. It will return as soon as possible.
 func (s *Service) Stop(svc service.Service) error {
+	// call global context shutdown
 	if s.shutdown != nil {
 		s.shutdown()
 	}
+
+	// shutdown HTTP server
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	err := s.srv.Shutdown(ctx)
+	if err == nil {
+		_ = s.log.Info("Configuration server stopped")
+	} else {
+		logger, lErr := svc.Logger(nil)
+		if lErr != nil {
+			_ = logger.Errorf("Failed to stop configuration server: %v", err)
+		}
+	}
+
 	return nil
+}
+
+func (s *Service) run(ctx context.Context) error {
+	// obtain latest version of the configuration
+	cfg, err := config.Load(ctx)
+	if err != nil {
+		_ = s.log.Errorf("Failed to load configuration: %v", err)
+		return err
+	}
+	_ = s.log.Infof("Starting service")
+
+	for {
+		// refresh configuration
+		if err := cfg.Refresh(); err != nil {
+			_ = s.log.Errorf("Failed to refresh configuration: %v", err)
+		}
+
+		// run the upload, IF the configuration is active
+		sleep := time.Second
+		if cfg.Active {
+			if err := Upload(ctx, cfg); err != nil {
+				_ = s.log.Errorf("While processing upload: %v", err)
+			}
+			sleep = cfg.Interval
+		}
+
+		// sleep until the next iteration
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(sleep):
+		}
+	}
 }

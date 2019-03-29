a744892fe31fac66a09318772ee312b96917eb87
Yigal Duppen <yigal@publysher.nl>
2019-03-29 09:44:34 +0100

    Add index limit to example query

--- data/queries/tg-voorstel.sql
+++ data/queries/tg-voorstel.sql
@@ -1,15 +1,3 @@
-WITH klacht AS
-       (
-         SELECT DISTINCT RIGHT(vav.objectid, 10) AS sehid,
-                         vkl1.omschr             AS klacht
-         FROM vrlijst_antwview vav
-                LEFT OUTER JOIN vrlijst_vragen vvr
-                                ON vav.realvrid = vvr.vraagid
-                LEFT OUTER JOIN vrlijst_keuzelst vkl1
-                                ON vvr.keuzelijst = vkl1.lijstcode AND SUBSTRING(vav.antwoord, 1, 10) = vkl1.code
-         WHERE vkl1.lijstcode = 'CS00000991'
-           AND vav.lijstid = 'CS00006105'
-       )
 SELECT TOP 100 mut.sehid      AS sehid,
                mut.sehmutid   AS sehmutid,
                reg.locatiecod AS locatie,
@@ -24,7 +12,7 @@ SELECT TOP 100 mut.sehid      AS sehid,
                reg.arbehetijd AS vertrektijd,
                mut.behkamerco AS kamer,
                mut.bednr      AS bed,
-               klacht.klacht  AS ingangsklacht,
+               reg.klacht  AS ingangsklacht,
                reg.specialism AS specialisme,
                reg.trianivcod AS triage,
                reg.vvcode     AS vervoerder,
@@ -34,12 +22,12 @@ SELECT TOP 100 mut.sehid      AS sehid,
                reg.vervoertyp AS herkomst,
                reg.bestemming AS ontslagbestemming
 FROM seh_sehmut mut
-       WITH (NOLOCK)
-       LEFT JOIN seh_sehreg reg ON mut.sehid = reg.sehid
-       LEFT OUTER JOIN klacht ON mut.sehid = klacht.sehid
-       LEFT OUTER JOIN patient_patient pp ON reg.patientnr = pp.patientnr
-       LEFT OUTER JOIN opname_opname oo ON reg.opnameid = oo.plannr
+         WITH (NOLOCK)
+         LEFT JOIN seh_sehreg reg ON mut.sehid = reg.sehid
+         LEFT OUTER JOIN patient_patient pp ON reg.patientnr = pp.patientnr
+         LEFT OUTER JOIN opname_opname oo ON reg.opnameid = oo.plannr
 WHERE reg.vervall = 0
+  AND reg.datum >= getdate() - 1
 ORDER BY mut.sehmutid DESC,
          reg.sehid DESC;
 
